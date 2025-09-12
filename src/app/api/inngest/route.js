98282d4e6b002738d15b9b45e77b74000599310b
Akintola97 <84879342+Akintola97@users.noreply.github.com>
2025-09-12 14:39:42 -0500

    scheduler

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -1,11 +1,13 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
 import newsletter from "@/inngest/news"
+import scheduler from "@/inngest/scheduler";
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-   newsletter
+   newsletter,
+   scheduler
   ],
 });
